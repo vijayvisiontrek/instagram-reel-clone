a26ce69eade2ca65841acadcca4866295334368c
vijay <vijay.kumar@visiontrek.com>
2021-12-25 20:32:39 +0530

    first commit

---
+++ login.js
@@ -0,0 +1,15 @@
+var name = 'amit';
+console.log(name)
+var marks = 2822;
+console.log(marks)
+
+
+
+const sname = 'beanty';
+console.log(sname)
+
+{
+let bname = 'rami';
+bname ='rams';
+}
+console.log(bname)
